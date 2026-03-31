b9b8533e09a9fdde15c0d73ea48eab27cc989e4d
hussain ullah <enghussainullah@gmail.com>
2026-03-31 08:42:17 +0500

    fix: MIME type error

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,24 @@ import express, { urlencoded } from "express";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 
-const app = express();
-
-app.use(express.json());
-app.use(urlencoded({ extended: true }))
+// web api
+import userWeb from './router/web/index.js';
+// api
+import userApi from './router/api/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
+const app = express();
+
+app.get('/public/css/output.css', (req, res) => {
+    res.type('text/css');
+    res.sendFile(__dirname + '/public/css/output.css');
+})
+
+
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
 
 // EJS setup — views inside src
 app.set("view engine", "ejs");
@@ -17,14 +28,10 @@ app.set("views", join(__dirname, "views"));
 // Static files — public outside src
 app.use(express.static(join(__dirname, "..", "public")));
 
+// Web routes
+app.use("/", userWeb);
 
-// views routes
-import userWeb from './router/web'
-app.use('/', userWeb)
-
-// apis
-import userApi from './router/api'
-app.use('/', userApi)
-
+// API routes
+app.use("/", userApi);
 
 app.listen(3000, () => console.log("Server running on port 3000"));
